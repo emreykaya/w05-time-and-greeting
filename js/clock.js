a061d8eca4b3541c72167733793895517596adf5
emreykaya <emrecanyerlikaya1@gmail.com>
2025-02-18 18:34:03 +0300

    hafta-05-ödev-1-saat-ve-karşılama ödevi tamamlandı.

--- js/clock.js
+++ js/clock.js
@@ -1,26 +1,25 @@
 // GREETING
+let fullName = prompt("Lütfen Adınızı Giriniz").trim();
+let myName = document.querySelector("#myName");
 
-let fullName = prompt("Lütfen Adınızı Giriniz")
-
-let myName = document.querySelector("#myName")
-
-myName.innerHTML = `${myName.innerHTML} ${fullName}`
-
-
-
-// CLOCK
-
-function showTime() {
-    let clockTime = new Date();
-    let hours = clockTime.getHours().toString().padStart(2,"0")
-    let minutes = clockTime.getMinutes().toString().padStart(2, "0");
-    let seconds = clockTime.getSeconds().toString().padStart(2, "0");
-    let days = ["Pazar","Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi"];
-    let dayName = days[clockTime.getDay()];
-    document.getElementById(`myClock`).textContent = `${hours}:${minutes}:${seconds}: ${dayName}`;
-    setTimeout(showTime, 1000);
+// GREETING - IF
+if (!fullName) {
+    fullName = "Ziyaretçi";
 }
 
-console.log(typeof new Date().getHours())
+myName.textContent = `Merhaba, ${fullName}!`;
+
+// CLOCK
+function showTime() {
+    let clockTime = new Date();
+    let hours = clockTime.getHours().toString().padStart(2, "0");
+    let minutes = clockTime.getMinutes().toString().padStart(2, "0");
+    let seconds = clockTime.getSeconds().toString().padStart(2, "0");
+    let days = ["Pazar", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi"];
+    let dayName = days[clockTime.getDay()];
+
+    document.getElementById("myClock").textContent = `${hours}:${minutes}:${seconds} - ${dayName}`;
+    setTimeout(showTime, 1000);
+}
 
 showTime();
